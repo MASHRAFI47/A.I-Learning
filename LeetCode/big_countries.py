772fe0ddc4cea29d5713cbc3779d48e28a7bc1a0
Mashrafi Bin Nur <mbngms@gmail.com>
2025-11-14 08:35:57 +0600

    leetcode

---
+++ LeetCode/big_countries.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    ans = world.loc[(world['area']>=3000000) | (world['population']>=25000000), ['name', 'population', 'area']]
+    return ans
